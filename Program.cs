7ea5b40b18368266d3f9fa8364c5c242a2a34ad6
Karin Oskarsson <k.oskarsson@outlook.com>
2023-11-07 17:11:31 +0100

    Bröt ut translate till staticmetod: WordTranslate

--- Program.cs
+++ Program.cs
@@ -98,25 +98,15 @@
                     {
                         foreach(SweEngGloss gloss in dictionary)
                         {
-                            if (gloss.word_swe == argument[1])
-                                Console.WriteLine($"English for {gloss.word_swe} is {gloss.word_eng}");
-                            if (gloss.word_eng == argument[1])
-                                Console.WriteLine($"Swedish for {gloss.word_eng} is {gloss.word_swe}");
+                            WordTranslate();
                         }
                     }
                     else if (argument.Length == 1)
                     {
-                        Console.WriteLine("Write word to be translated: ");
-                        string s = Console.ReadLine();
-                        foreach (SweEngGloss gloss in dictionary)
-                        {
-                            if (gloss.word_swe == s)
-                                Console.WriteLine($"English for {gloss.word_swe} is {gloss.word_eng}");
-                            if (gloss.word_eng == s)
-                                Console.WriteLine($"Swedish for {gloss.word_eng} is {gloss.word_swe}");
+                            Console.WriteLine("Write word to be translated: ");
+                            WordTranslate();
                         }
                     }
-                }
                 else
                 {
                     Console.WriteLine($"Unknown command: '{command}'");
@@ -124,8 +114,18 @@
             }
             while (true);
         }
-
-        private static void LoadList(StreamReader sr)
+        private static void WordTranslate()
+        {
+            string s = Console.ReadLine();
+            foreach (SweEngGloss gloss in dictionary)
+            {
+                if (gloss.word_swe == s)
+                    Console.WriteLine($"English for {gloss.word_swe} is {gloss.word_eng}");
+                if (gloss.word_eng == s)
+                    Console.WriteLine($"Swedish for {gloss.word_eng} is {gloss.word_swe}");
+            }
+        }
+        static void LoadList(StreamReader sr)
         {
             dictionary = new List<SweEngGloss>(); // Empty it!
             string line = sr.ReadLine();
